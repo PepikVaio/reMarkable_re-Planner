01b054a213dc6a4be4e44f142130ec5e1f9b8684
Wajsar Josef <36114753+PepikVaio@users.noreply.github.com>
2024-01-04 21:48:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 document.getElementById('showQRButton').addEventListener('click', function() {
     var enteredCode = document.getElementById('codeInput').value;
-    var correctCode = 'testovyKoda';  // Nahraďte skutečným kódem
+    var correctCode = 'testovyKod';  // Nahraďte skutečným kódem
 
     if (enteredCode === correctCode) {
-        document.getElementById('qrCodeContainer').style.display = 'block';
+        var qrCodeContainer = document.getElementById('qrCodeContainer');
+        qrCodeContainer.innerHTML = '<img src="https://placekitten.com/200/200" alt="Nový QR kód">';
+        qrCodeContainer.style.display = 'block';
     } else {
         alert('Nesprávný kód. Zkuste to znovu.');
     }
