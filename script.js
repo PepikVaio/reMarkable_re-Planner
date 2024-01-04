8d1029c0b1055c2897d57fe87b4d21a9430722f0
Wajsar Josef <36114753+PepikVaio@users.noreply.github.com>
2024-01-04 21:49:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('showQRButton').addEventListener('click', function() {
     var enteredCode = document.getElementById('codeInput').value;
-    var correctCode = 'testovyKod';  // Nahraďte skutečným kódem
+    var correctCode = 'test';  // Nahraďte skutečným kódem
 
     if (enteredCode === correctCode) {
         var qrCodeContainer = document.getElementById('qrCodeContainer');
