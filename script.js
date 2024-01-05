8c04e6711ee330521e49e96168d5fcdee11cf6a7
Wajsar Josef <36114753+PepikVaio@users.noreply.github.com>
2024-01-05 19:41:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 document.getElementById('showQRButton').addEventListener('click', function() {
     var enteredCode = document.getElementById('codeInput').value;
-    var correctCode = 'kocka';  // Nahraďte skutečným kódem
+    var correctCode = 'testovyKod';  // Nahraďte skutečným kódem
 
     if (enteredCode === correctCode) {
-        var qrCodeContainer = document.getElementById('qrCodeContainer');
-        qrCodeContainer.innerHTML = '<img src="https://placekitten.com/200/200" alt="Nový QR kód">';
-        qrCodeContainer.style.display = 'block';
+        // Otevření vyskakovacího okna
+        document.getElementById('myModal').style.display = 'block';
     } else {
         alert('Nesprávný kód. Zkuste to znovu.');
+    }
+});
+
+// Zavření vyskakovacího okna kliknutím na ikonu X
+document.querySelector('.close').addEventListener('click', function() {
+    document.getElementById('myModal').style.display = 'none';
+});
+
+// Zavření vyskakovacího okna kliknutím mimo obsah
+window.addEventListener('click', function(event) {
+    var modal = document.getElementById('myModal');
+    if (event.target === modal) {
+        modal.style.display = 'none';
     }
 });
