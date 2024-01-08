e5274196df7b19cfc93d78c5af9123d9fa6573e5
Wajsar Josef <36114753+PepikVaio@users.noreply.github.com>
2024-01-08 23:06:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
 document.getElementById('showQRButton').addEventListener('click', function() {
     var enteredCode = document.getElementById('codeInput').value;
+    var correctCode = 'testovyKod';  // Nahraďte skutečným kódem
 
-    // Volání API pro získání správného kódu
-    fetch('/api/getCorrectCode')  // Nahraďte skutečnou cestou k API
-        .then(response => response.json())
-        .then(data => {
-            var correctCode = data.correctCode;
-
-            if (enteredCode === correctCode) {
-                // Otevření vyskakovacího okna
-                document.getElementById('myModal').style.display = 'block';
-            } else {
-                alert('Nesprávný kód. Zkuste to znovu.');
-            }
-        })
-        .catch(error => {
-            console.error('Chyba při získávání kódu:', error);
-        });
+    if (enteredCode === correctCode) {
+        // Otevření vyskakovacího okna
+        document.getElementById('myModal').style.display = 'block';
+    } else {
+        alert('Nesprávný kód. Zkuste to znovu.');
+    }
 });
 
 // Zavření vyskakovacího okna kliknutím na ikonu X
