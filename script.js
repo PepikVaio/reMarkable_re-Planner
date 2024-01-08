389e2bc4572752c35773d70edc717438131b8e38
Wajsar Josef <36114753+PepikVaio@users.noreply.github.com>
2024-01-08 23:03:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 document.getElementById('showQRButton').addEventListener('click', function() {
     var enteredCode = document.getElementById('codeInput').value;
-    
-    // Nahraďte následující řádek načtením citlivé informace ze tajemství
-    var correctCode = process.env.MY_SECRET_CODE;  // Nahraďte názvem vašeho tajemství
 
-    if (enteredCode === correctCode) {
-        // Otevření vyskakovacího okna
-        document.getElementById('myModal').style.display = 'block';
-    } else {
-        alert('Nesprávný kód. Zkuste to znovu.');
-    }
+    // Volání API pro získání správného kódu
+    fetch('/api/getCorrectCode')  // Nahraďte skutečnou cestou k API
+        .then(response => response.json())
+        .then(data => {
+            var correctCode = data.correctCode;
+
+            if (enteredCode === correctCode) {
+                // Otevření vyskakovacího okna
+                document.getElementById('myModal').style.display = 'block';
+            } else {
+                alert('Nesprávný kód. Zkuste to znovu.');
+            }
+        })
+        .catch(error => {
+            console.error('Chyba při získávání kódu:', error);
+        });
 });
 
 // Zavření vyskakovacího okna kliknutím na ikonu X
