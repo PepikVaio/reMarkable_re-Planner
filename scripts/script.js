81d77ffc10c8623171d240d3171fd4695325cc7a
Wajsar Josef <36114753+PepikVaio@users.noreply.github.com>
2024-01-09 22:13:02 +0100

    update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,28 @@
-document.getElementById('showQRButton').addEventListener('click', function() {
-    var enteredCode = document.getElementById('codeInput').value;
-    var correctCode = 'kocka';  // Nahraďte skutečným kódem
+// Kontrola, zda je objekt document definován (pracujeme ve webovém prostředí)
+if (typeof document !== 'undefined') {
+    // Kód pracující s DOMem
+    document.getElementById('showQRButton').addEventListener('click', function() {
+        var enteredCode = document.getElementById('codeInput').value;
+        var correctCode = 'kocka';  // Nahraďte skutečným kódem
 
-    if (enteredCode === correctCode) {
-        // Otevření vyskakovacího okna
-        document.getElementById('myModal').style.display = 'block';
-    } else {
-        alert('Nesprávný kód. Zkuste to znovu.');
-    }
-});
+        if (enteredCode === correctCode) {
+            // Otevření vyskakovacího okna
+            document.getElementById('myModal').style.display = 'block';
+        } else {
+            alert('Nesprávný kód. Zkuste to znovu.');
+        }
+    });
 
-// Zavření vyskakovacího okna kliknutím na ikonu X
-document.querySelector('.close').addEventListener('click', function() {
-    document.getElementById('myModal').style.display = 'none';
-});
+    // Zavření vyskakovacího okna kliknutím na ikonu X
+    document.querySelector('.close').addEventListener('click', function() {
+        document.getElementById('myModal').style.display = 'none';
+    });
 
-// Zavření vyskakovacího okna kliknutím mimo obsah
-window.addEventListener('click', function(event) {
-    var modal = document.getElementById('myModal');
-    if (event.target === modal) {
-        modal.style.display = 'none';
-    }
-});
-
-
+    // Zavření vyskakovacího okna kliknutím mimo obsah
+    window.addEventListener('click', function(event) {
+        var modal = document.getElementById('myModal');
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+}
