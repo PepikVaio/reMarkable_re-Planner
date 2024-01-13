96ef18a95352f9ec4c058487a5217638ef37e009
Wajsar Josef <36114753+PepikVaio@users.noreply.github.com>
2024-01-13 15:35:16 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,5 @@
-if (typeof document !== 'undefined') {
-    // Kód pracující s DOMem
-    document.getElementById('showQRButton').addEventListener('click', function() {
-        var enteredCode = document.getElementById('codeInput').value;
-        var correctCode = window.MY_SECRET_CODE || '';  // Použití globální proměnné
+// Získání citlivé informace z proměnných prostředí
+const secretPassword = process.env.MOJE_TAJNE_HESLO;
 
-        if (enteredCode === correctCode) {
-            // Otevření vyskakovacího okna
-            document.getElementById('myModal').style.display = 'block';
-        } else {
-            alert('Nesprávný kód. Zkuste to znovu.');
-        }
-    });
-
-    // Zavření vyskakovacího okna kliknutím na ikonu X
-    document.querySelector('.close').addEventListener('click', function() {
-        document.getElementById('myModal').style.display = 'none';
-    });
-
-    // Zavření vyskakovacího okna kliknutím mimo obsah
-    window.addEventListener('click', function(event) {
-        var modal = document.getElementById('myModal');
-        if (event.target === modal) {
-            modal.style.display = 'none';
-        }
-    });
-}
+// Dále můžete použít tuto proměnnou v kódu
+console.log(secretPassword);
