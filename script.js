c48734c10370db9056eb565d8fa064ae4e62cb57
Wajsar Josef <36114753+PepikVaio@users.noreply.github.com>
2024-01-13 16:12:12 +0100

    delete

--- script.js
+++
@@ -1,7 +0,0 @@
-function showPassword() {
-  // Zakódované heslo (pouze pro demonstrační účely)
-  const secretPassword = "MojeTajneHeslo";
-
-  // Vypsání hesla na obrazovku (pouze pro demonstrační účely)
-  document.getElementById('passwordContainer').innerText = `Heslo: ${secretPassword}`;
-}
